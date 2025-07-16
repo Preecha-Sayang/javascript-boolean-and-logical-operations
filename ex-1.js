8d23b9b400e2465a4913873463d02e1d831c3ffe
lalinyuuu <lullalin.yu@gmail.com>
2025-07-16 15:50:25 +0700

    feat: update exercises with initial conditions and debugging adjustments

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,10 @@
-let isOver18;
-let hasCriminalBlacklist;
-
-let isAllow;
+// Exercise #1: Event Conditions
 
 // Start coding here
+// jame
+let isOver18 = false;
+let hasCriminalBlacklist = false;
+
+let isAllow = isOver18 && !hasCriminalBlacklist;
 
 console.log(isAllow);
